f1f3bd59ae52bed8a034620494502010e2ac1cf3
N-eeraj <neeraj7rajeena@gmail.com>
2022-04-30 16:52:02 +0530

    added settings functionality & portrait UI

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = Vue.createApp({
 			levels: null,
 			audios: null,
 			currentAudio: null,
-			darkmode: false
+			currentTab: null,
+			highScores: null,
+			storage: null,
 		}
 	},
 	mounted() {
@@ -17,28 +19,71 @@ const app = Vue.createApp({
 			this.categories = data.categories
 			this.levels = data.levels
 			this.audios = data.sound
+			this.storage = JSON.parse(localStorage.getItem("riddleMeThis"))
+			if (this.storage === null)
+				this.storage = {
+								scores: {},
+								darkmode: false,
+								sound: false
+							}
 			this.currentAudio = new Audio(this.audios.bgm)
-			setTimeout(() => this.loaded = true, 3000)
+			setTimeout(() => this.loaded = true, 1000)
 		})
 	},
 	methods: {
 		closeSplashScreen() {
 			if (this.loaded) {
 				this.splashScreen = false
-				this.currentAudio.play()
+				this.currentTab = 0
+				this.currentAudio.loop = true
+				if (this.storage.darkmode) {
+					this.storage.darkmode = false
+					this.toggleDarkMode()
+				}
+				if (this.storage.sound)
+					this.currentAudio.play()
 			}
 		},
-		toggleMode() {
-			this.darkmode = !this.darkmode
-			if (this.darkmode)
-				document.body.classList.add("dark-mode")
-			else
-				document.body.classList.remove("dark-mode")
+		updateSettings(action) {
+			switch (action) {
+				case "soundToggle":
+					this.toggleSound()
+					break
+				case "darkModeToggle":
+					this.toggleDarkMode()
+					break
+				case "reset":
+					this.storage.scores = {}
+					break
+			}
+			this.updateLocalStorage()
+		},
+		toggleSound() {
+			if (this.currentAudio.paused) {
+				this.storage.sound = true
+				return this.currentAudio.play()
+			}
+			this.storage.sound = false
+			return this.currentAudio.pause()
+		},
+		toggleDarkMode() {
+			this.storage.darkmode = !this.storage.darkmode
+			if (this.storage.darkmode)
+				return document.body.classList.add("dark-mode")
+			return document.body.classList.remove("dark-mode")
+		},
+		updateLocalStorage() {
+			localStorage.setItem("riddleMeThis", JSON.stringify(this.storage))
 		}
 	},
 	template: `
 		<splash-screen v-if="splashScreen" :loading="loaded" @click="closeSplashScreen" />
-		<main-menu />
+		<template v-if="!splashScreen">
+			<main-nav @selectTab="tabNum => this.currentTab = tabNum" />
+			<play v-if="currentTab===0" />
+			<highscores v-else-if="currentTab===1" />
+			<settings v-else @changeSettings="updateSettings" :options="storage" />
+		</template>
 	`
 })
 
@@ -56,15 +101,82 @@ app.component("splash-screen", {
 })
 
 
-app.component("main-menu", {
+app.component("main-nav", {
+	methods: {
+		changeTab(tab) {
+			return this.$emit("selectTab", tab)
+		}
+	},
 	template: `
 		<nav>
+			<h1>Riddle Me This</h1>
 			<ul>
-				<li>Home</li>
-				<li>High Scores</li>
-				<li>Settings</li>
+				<li @click="changeTab(0)">
+					<i class="fa-solid fa-home"></i>
+					<span>Home</span>
+				</li>
+				<li @click="changeTab(1)">
+					<i class="fa-solid fa-trophy"></i>
+					<span>High Scores</span>
+				</li>
+				<li @click="changeTab(2)">
+					<i class="fa-solid fa-gear"></i>
+					<span>Settings</span>
+				</li>
 			</ul>
 		</nav>
+	`
+})
+
+
+app.component("play", {
+	template: `
+		<main>
+			<h2>Choose Category</h2>
+		</main>
+	`
+})
+
+
+app.component("highscores", {
+	template: `
+		<main>
+			<h2>High Scores</h2>
+		</main>
+	`
+})
+
+
+app.component("settings", {
+	methods: {
+		settingsAction(action) {
+			this.$emit('changeSettings', action)
+		}
+	},
+	props: ["options"],
+	template: `
+		<main>
+			<h2>Settings</h2>
+			<ul class="settings">
+				<li class="row">
+					<label for="soundOn">
+						<span>Sound</span>
+						<input type="checkbox" id="soundOn" :checked="options.sound?true:false" @change="settingsAction('soundToggle')">
+						<div class="switch"></div>
+					</label>
+				</li>
+				<li class="row">
+					<label for="darkMode">
+						<span>Darkmode</span>
+						<input type="checkbox" id="darkMode" :checked="options.darkmode?true:false" @change="settingsAction('darkModeToggle')">
+						<div class="switch"></div>
+					</label>
+				</li>
+				<li class="row">
+					<button @click="settingsAction('reset')">Reset High Scores</button>
+				</li>
+			</ul>
+		</main>
 	`
 })
 
